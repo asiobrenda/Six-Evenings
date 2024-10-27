e7ecea0a5457dbc02dbfa450bfa4b95822ddc7e4
Asio Brenda <asiobrenda01@gmail.com>
2024-10-27 21:29:03 +0300

    modified settings

--- makeup/settings.py
+++ makeup/settings.py
@@ -170,8 +170,12 @@ MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "makeup/static"), ]
 
+if not DEBUG:  # Production environment
+    MEDIA_ROOT = '/var/www/media'
+else:  # Development environment
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 STATIC_ROOT = os.path.join(MAK_DIR, "static")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
