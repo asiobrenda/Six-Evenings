4f2ee0c367f623cb2537468165818314fa654d91
Asio Brenda <asiobrenda01@gmail.com>
2024-10-29 12:37:41 +0300

    modified settings

--- makeup/urls.py
+++ makeup/urls.py
@@ -24,9 +24,5 @@ urlpatterns = [
     path('', include('makeup.apps.dating.urls')),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # Serve media files in production explicitly
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
