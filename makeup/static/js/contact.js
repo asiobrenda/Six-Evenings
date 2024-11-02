14b32f67e3de057aab8ed46b86d79d625329f68b
Asio Brenda <asiobrenda01@gmail.com>
2024-11-02 20:49:45 +0300

    modified alot of files to include mobile phones

--- makeup/static/js/contact.js
+++ makeup/static/js/contact.js
@@ -1,27 +1,34 @@
-function send_message(event){
-  event.preventDefault()
-  var name_ = document.getElementById('name').value;
-  var email_ = document.getElementById('email').value;
-  var message_ = document.getElementById('message').value;
+function send_message(event) {
+    // First, let the browser check the form validation
+    if (!document.getElementById('myform').checkValidity()) {
+        return; // If the form is invalid, do nothing
+    }
+
+    event.preventDefault();
+
+    // Get values from form fields
+    var name_ = document.getElementById('name').value.trim();
+    var email_ = document.getElementById('email').value.trim();
+    var message_ = document.getElementById('message').value.trim();
+
+    // Send AJAX request since all fields are valid
     $.post(contactUs,
         {
-              'name': name_,
-              'email': email_,
-              'message': message_
+            'name': name_,
+            'email': email_,
+            'message': message_
         },
-        function(data){
+        function(data) {
             if (data.status === 'success') {
-               document.getElementById('myform').reset();
-               var toast = document.getElementById('toast');
+                document.getElementById('myform').reset();
+                var toast = document.getElementById('toast');
                 toast.style.display = 'block';
                 setTimeout(function() {
-                  toast.style.display = 'none';
+                    toast.style.display = 'none';
                 }, 1000);
             } else {
                 alert('Error: ' + data.message);
             }
-
         }
-    )
-
+    );
 }
