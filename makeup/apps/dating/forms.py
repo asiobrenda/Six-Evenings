8830266e3c7009dc32b745da1560cd4645ec9e4c
Asio Brenda <asiobrenda01@gmail.com>
2024-11-12 19:02:43 +0300

    worked on the sign up form

--- makeup/apps/dating/forms.py
+++ makeup/apps/dating/forms.py
@@ -2,11 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import SignUpUser
 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import SignUpUser
+
 class SignUpCreationForm(UserCreationForm):
     class Meta:
         model = SignUpUser
         fields = ('username', 'email', 'password1', 'password2')
         label_suffix = ''
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpCreationForm, self).__init__(*args, **kwargs)
+        # Add a placeholder for the username field with an example
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'e.g., user123, john_doe, jane.doe23'
+        })
 
     def save(self, commit=True):
         user = super().save(commit=False)
